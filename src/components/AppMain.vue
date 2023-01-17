1cd39327f5798927564ce1cf253fd25d17ea4fd7
MyUsername <my@email.com>
2023-01-17 11:13:25 +0100

    test for flag

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,6 +12,14 @@ export default {
          store,
       }
    },
+
+   methods: {
+      getImagePath(imgFlag){
+         if (imgFlag !== 'da' && imgFlag !== 'de' && imgFlag !== 'en' && imgFlag !== 'fr' && imgFlag !== 'hi' && imgFlag !== 'it' && imgFlag !== 'ja' && imgFlag !== 'zh'){
+            return new URL(`../assets/img/${imgFlag}.png`, import.meta.url).href;
+         }
+      }
+   },
 }
 </script>
 
@@ -45,6 +53,7 @@ export default {
          <h3>
             {{ tvShow.original_name }}
          </h3>
+         <p>Language: <img class="flagImg" :src="getImagePath(movie.original_language)" alt="Non disponibile"></p>
          <p>Rating Tv Shows: <i class="fa-solid fa-star" v-for="n in Math.ceil(tvShow.vote_average / 2)"></i>
          <i class="fa-regular fa-star" v-for="n in Math.floor(5 - (tvShow.vote_average / 2))"></i>   
          </p>
@@ -67,5 +76,9 @@ export default {
    text-align: center;
    color: rgb(255, 255, 255);
    padding: 2rem;
+   }
+
+   .flagImg{
+      height: 40px;
    }
 </style>
